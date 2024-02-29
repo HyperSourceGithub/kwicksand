73ce3b01a9c725cba90b03b6890faefdb4d0ca50
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-02-29 15:04:35 +0000

    Update kwik.py with text engine and made ideas.md

---
+++ ideas.md
@@ -0,0 +1,12 @@
+# ideas
+ok how is this gonna work?
+uhhhh
+ok
+
+## trust the [process]
+get input from function kwik(text)
+for letter in the length of text, add it to a list
+#### loop: [repeat for length of text]
+clear last line ->
+write letters based on index ->
+#### end loop
--- kwik.py
+++ kwik.py
@@ -1,1 +1,18 @@
+import time
 
+txt = [] # define a list for the text
+
+def kwik(text):
+    txt.clear()
+    for l in text:
+        txt.append(l)
+    for i in range(len(txt)):
+        print(*txt[0:i], sep="", end='\r')
+        time.sleep(0.08)
+    print(*txt[0:len(txt)], sep="")
+
+# tests
+print("KWIKSAND TEST")
+kwik("Hello World!")
+kwik("I am Bob!")
+kwik("This is a text engine.")
