f4926773dd4061425840e65614121334ad5cff10
HyperSource <135276870+HyperSourceGithub@users.noreply.github.com>
2024-03-05 14:50:29 +0000

    Played around with different methods. (func.py and kwiksand.py are not synced yet)

--- TESTS/func.py
+++ TESTS/func.py
@@ -1,6 +1,19 @@
 import time
+import sys
 
 txt = [] # define a list for the text
+
+def delete_lines(n):
+    for _ in range(n):
+        sys.stdout.write("\x1b[1A")  # cursor up one line
+        sys.stdout.write("\x1b[2K")  # delete the last line
+
+def testkwik(text):
+    for i in range(len(text)):
+        print(text[0:i])
+        time.sleep(0.08)
+        delete_lines(1)
+    print(text)
 
 def kwik(text):
     txt.clear()
@@ -8,6 +21,17 @@ def kwik(text):
         txt.append(l)
     for i in range(len(txt)):
         print(*txt[0:i], sep="", end='\r')
+        time.sleep(0.08)
+    print(*txt[0:len(txt)], sep="")
+
+def syskwik(text):
+    txt.clear()
+    for l in text:
+        txt.append(l)
+    for i in range(len(txt)):
+        print(*txt[0:i], sep="") #, end='\r')
+        sys.stdout.write("\x1b[1A")
+        sys.stdout.write("\x1b[2K")
         time.sleep(0.08)
     print(*txt[0:len(txt)], sep="")
 
--- TESTS/singlekwik.py
+++ TESTS/singlekwik.py
@@ -7,8 +7,28 @@ thing = "really long text to test the kwik system. the kwik system effectively c
 # main loop
 os.system('clear')
 print("Welcome to the kwiksand environment!")
-func.kwiklist()
-time.sleep(3)
-func.kwik(thing)
+print("This is a test of the kwik system.")
+
+for i in range(3):
+    print(f"Resetting in {3-i}...")
+    time.sleep(1)
+    func.delete_lines(1)
+print("Clearing lines...")
+time.sleep(1)
+func.delete_lines(3)
+time.sleep(1)
+
+func.kwik(f"[1] {thing}")
+time.sleep(1)
+os.system('clear')
+
+func.syskwik(f"[2] {thing}")
+time.sleep(1)
+os.system('clear')
+
+func.testkwik(f"[3] {thing}")
+time.sleep(1)
+os.system('clear')
+
 func.kwiklist()
 func.kwiklist_str()
