08aa94e143410a698a1a014a0c92d1c387867a51
HyperSource <lucian@raileanu.ca>
2024-11-01 14:11:04 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='kwiksand',
     packages=find_packages(),
-    version='0.1.1',
+    version='0.1.2',
     description='a thing to write text charachter by character',
     author='HyperSource01',
     long_description=open('README.md').read(),
