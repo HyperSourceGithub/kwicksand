5a516d7a7aff55a0627dac9de49639516ff76830
HyperSource <lucian@raileanu.ca>
2024-11-01 11:09:15 -0400

    Use shutil to verif text length

--- kwiksand.py
+++ kwiksand.py
@@ -1,15 +1,19 @@
 import time
+import shutil
 
 txt = [] # define a list for the text
 
 def kwik(text):
-    txt.clear()
-    for l in text:
-        txt.append(l)
-    for i in range(len(txt)):
-        print(*txt[0:i], sep="", end='\r')
-        time.sleep(0.08)
-    print(*txt[0:len(txt)], sep="")
+    if len(text) >= shutil.get_terminal_size().columns:
+        print("ERR: Text too long!")
+    else:
+        txt.clear()
+        for l in text:
+            txt.append(l)
+        for i in range(len(txt)):
+            print(*txt[0:i], sep="", end='\r')
+            time.sleep(0.08)
+        print(*txt[0:len(txt)], sep="")
 
 def kwiklist():
     print(txt)
