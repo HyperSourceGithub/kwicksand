8ef1759cea742afb837169864348bb416791ad47
HyperSource <lucian@raileanu.ca>
2024-11-02 11:42:38 -0400

    ONE point zero point zero is ready!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='kwiksand',
     packages=find_packages(include=['kwiksand']),
-    version='0.1.5',
+    version='1.0.0',
     description='a thing to write text character by character',
     author='HyperSource01',
     long_description=open('README.md').read(),
