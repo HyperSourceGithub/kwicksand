88c4b7b08ddd47839911e9197dfa32aa32db19c9
HyperSource <lucian@raileanu.ca>
2024-11-11 19:22:23 +0000

    Removed tests and added "overprint" parameter

--- kwiksand/kwiksand.py
+++ kwiksand/kwiksand.py
@@ -1,11 +1,14 @@
 import time
 import shutil
 
-def kwik(text, delay=0.04):
+def kwik(text, delay=0.04, overprint=False):
     global txt
     txt = []
-    if len(text) > shutil.get_terminal_size().columns:
+    if len(text) > shutil.get_terminal_size().columns and overprint == False:
         print(f"ERR: Text too long! Your text is {len(text)} characters while your output can only print {shutil.get_terminal_size().columns} characters.")
+        print("To circumvent this, set the \'overprint\' property to True.")
+    elif len(text) > shutil.get_terminal_size().columns and overprint == True:
+        print(text)
     else:
         txt.clear()
         for l in text:
